02e549ae66e1fca77589b6ef676617188154a512
dianakw8591 <dianakw@gmail.com>
2020-03-23 16:00:05 -0700

    changed to postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_23_221931) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guesses", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "item_id", null: false
