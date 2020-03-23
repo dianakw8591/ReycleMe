f9c64fcb17a55fc9f9539ee147617a06b5e9b7b1
dianakw8591 <dianakw@gmail.com>
2020-03-23 15:21:55 -0700

    resources for user, item, guesses, models have appropriate relationships

---
+++ db/migrate/20200323221727_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :type
+      t.string :subcategory
+      t.string :note
+
+      t.timestamps
+    end
+  end
+end
