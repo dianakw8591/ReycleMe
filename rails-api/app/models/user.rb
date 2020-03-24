05f35305f2fd0ff702f92181104f8909e3f14b34
dianakw8591 <dianakw@gmail.com>
2020-03-24 11:46:42 -0700

    user backend: password encryption, routes for creating user and user login

--- rails-api/app/models/user.rb
+++ rails-api/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :guesses
     has_many :items, through: :guesses
+
+    has_secure_password
+
+    validates :username, {presence: true, uniqueness: true}
+    validates :password, {presence: true, on: :create}
 end
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :guesses
   resources :items
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'users#login'
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
