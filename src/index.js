eabde41dd535369ce7f0270fee6ee1eee5d0e1d1
Agandaur <cjwagstaffe@gmail.com>
2020-03-25 11:48:22 -0700

    first shot at edit and delete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const ITEMS_URL = "PLACE URL HERE";
 const STATS_URL = "PLACE URL HERE";
-const USERS_URL = "PLACE URL HERE";
+
+const USER_ID = "";
+const USER_URL = `localhost:3000/api/v1/${USER_ID}`;
 
 document.addEventListener("DOMContentLoaded", function() {
     buildUserLoginForm();
-    buildUserRegisterForm();
+    
     buildUserEditForm();
     buildUserDeleteAction();
     getStats();
@@ -15,12 +17,55 @@ function buildUserLoginForm () {
 
 }
 
+
 function buildUserEditForm() {
-    const user_name = document.getElementById("INPUT CORRECT ELEMENT ID")
+    const editDiv = document.getElementById("INPUT CORRECT ELEMENT ID")
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit"
+    editButton.addEventListener("click", function() {
+        const editForm = document.createElement("form")
+        editForm.setAttribute("method", "patch");
+        const header = document.createElement("h3")
+        header.innerText = "Update Your Information"
+        const nameLabel = document.createElement("label")
+        nameLabel.innerText = "Name :"
+        const nameField = document.createElement("inut")
+        nameField.name = "userName"
+        const editSubmit = document.createElement("button")
+        editSubmit.type = "submit"
+        editSubmit.innerText = "Update Information"
+
+
+        editForm.appendChild(header)
+        editForm.appendChild(nameLabel)
+        editForm.appendChild(nameField)
+        editForm.appendChild(editSubmit)
+
+        editForm.addEventListener("submit", function(event) {
+            //call a function to edit
+        })
+
+    })
+
+    editDiv.appendChild(editButton);
 }
 
 function buildUserDeleteAction () {
+    const deleteDiv = document.getElementById("INPUT CORRECT ELEMENT ID")
+    const delteButton = document.createElement("button")
+    deleteButton.innerText = "Delete"
+    deleteButton.addEventListener("click", function() {
+        deleteUser();
+    })
 
+    deleteDiv.appendChild(deleteButton);
+}
+
+function deleteUser() {
+    fetch(USER_URL, {
+        method: "DELETE"
+    })
+    .then(buildUserSignupForm) //double check this function matches function Diana is building
 }
 
 function getStats() {
