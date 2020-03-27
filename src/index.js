54fe27ea62d94ee1055640fe29e03fde3a3974e4
Agandaur <cjwagstaffe@gmail.com>
2020-03-26 21:20:58 -0700

    built response div

--- src/index.js
+++ src/index.js
@@ -87,19 +87,14 @@ function makeThePage() {
     addHeaderTitle();
     //all the functions go in here
     buildItemForm();
-    
-
     buildUserEditForm();
     buildUserDeleteAction();
     buildUserLogout();
     getStats();
-    
-    // buildItemForm();
 }
 
 
 function buildLoginButton() {
-    console.log("TEST")
     const button = document.createElement("button");
     button.id = "login";
     button.innerText = "Login"
@@ -437,9 +432,11 @@ function showStats(data) {
 
     const correctcount = document.createElement("h3");
     correctcount.innerText = `Correct: ${data.correct}`;
+    correctcount.id = "correct_id"
 
     const incorrectcount = document.createElement("h3");
     incorrectcount.innerText = `Incorrect: ${data.incorrect}`;
+    incorrectcount.id = "incorrect_id"
 
     
     analyticsBox.appendChild(h1);
@@ -450,8 +447,8 @@ function showStats(data) {
 }
 
 function buildItemForm() {
-const searchForm = document.createElement("form")
-    searchForm.setAttribute("method", "patch");
+    const searchForm = document.createElement("form")
+    
     const header = document.createElement("h3")
     header.innerText = "What do you want to recycle"
     const searchLabel = document.createElement("label")
@@ -464,7 +461,8 @@ const searchForm = document.createElement("form")
     const pickType = document.createElement("h4")
     pickType.innerHTML = "What category does this item fit into?"
     const kinds = document.createElement("div")
-    kinds.id=kinds
+    kinds.name = "options"
+    kinds.id = "options"
     const recy = document.createElement("input")
     recy.setAttribute("type", "radio");
     recy.label = "Recycling"
@@ -505,21 +503,52 @@ const searchForm = document.createElement("form")
     kinds.appendChild(cLabel)
     searchForm.appendChild(kinds)
     search.appendChild(searchForm)
+
+    searchForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        createGuess(event)
+    })
+}
+
+function createGuess(event) {
+    const input = event.target.searchItem.value
+    const radioButtonSelection = event.target.general_type.value
+    const item_id = 1 //ADD IN ITEM ID ONCE WE HAVE IT
+    const guess = {"guess": {
+        "user_id": `${user_id}`, "item_id": `${item_id}`, "guessed_category": `${radioButtonSelection}`
+        }
+    }
+    fetch(BASE_URL + "guesses", {
+        method: "POST",
+        // mode: "no-cors",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        },
+        body: JSON.stringify(guess)
+    })
+    .then(res => res.json())
+    .then (json => {
+        buildResponse(json)
+    })
+
 }
 
 function buildResponse(guessInfo) {
-    const guessDiv = getElementById("INPUT CORRECT ID") //get appropraite element from form once complete
+    const searchDiv = document.getElementById('search_div') 
     const responseDiv = document.createElement("div")
-    const responseHeader = document.createElement("h3")
+    const responseHeader = document.createElement("h4")
     
-    //confirm what comes back from guessInfo once form is complete
     if (guessInfo["data"]["attributes"].correct == true) {
         responseHeader.innerText = "You got it right!"
+        let numToIncrease = document.getElementById("correct_id").innerText.split(" ")[1]
+        document.getElementById("correct_id").innerText = "Correct: " + (parseInt(numToIncrease) +1)
     } else { 
         responseHeader.innerText = "Not quite. Try again next time!"
+        let numToIncrease = document.getElementById("incorrect_id").innerText.split(" ")[1]
+        document.getElementById("incorrect_id").innerText = "Incorrect: " + (parseInt(numToIncrease) +1)
     }
     
-    //confirm what comes back from guessInfo once form is complete
     const guessSection = guessInfo["included"][0]["attributes"]
     const responseText = document.createElement("p")
     if (guessSection.general_type == "recycling") {
@@ -533,14 +562,13 @@ function buildResponse(guessInfo) {
     responseDiv.appendChild(responseHeader)
     responseDiv.appendChild(responseText)
     
-    //confirm what comes back from guessInfo once form is complete
     if (guessSection.note) {
         const responseNote = document.createElement("p")
         responseNote.innerText = guessSection.note
         responseDiv.appendChild(responseNote)
     }
 
-    guessDiv.appendChild(responseDiv)
+    searchDiv.appendChild(responseDiv)
 }
 
 function deleteChildren(parent) {
