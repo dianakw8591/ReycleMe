49cd16769906f42519b09736f3285e7f775bb99c
dianakw8591 <dianakw@gmail.com>
2020-03-27 09:04:44 -0700

    moved items routes into api/v1 namespace

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :items
   namespace :api do
     namespace :v1 do
       post '/login', to: 'users#login'
       get '/users/:id/guesses', to: 'users#guesses'
       resources :users
       resources :guesses
+      resources :items
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
